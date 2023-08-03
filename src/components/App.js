25ffcfeb91f1b525db92cf9b64ddd46dbf0da07d
RJ <randal.sartor@gmail.com>
2023-08-03 13:53:32 -0400

    fix: dashboard import

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import LandingPage from './Landing';
-import Dashboard from './Dashboard';
+import Dash from './Dash';
 import LoginForm from './LoginForm';
 import ClaimPlate from './ClaimPlate';
 import PublicPlate from './PublicPlate';
@@ -16,7 +16,7 @@ import { Auth0Provider } from '../auth/Auth0Provider';
 const routesArray = [
   { path: '/', element: <LandingPage /> },
   // { path: "/register", element: <RegistrationPage /> },
-  { path: '/dashboard', element: <Dashboard /> },
+  { path: '/dashboard', element: <Dash /> },
   { path: '/login', element: <LoginForm /> },
   { path: '/plate', element: <PublicPlate /> },
   { path: '/plate/id/:id', element: <PublicPlate /> },
